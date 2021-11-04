f5d12783db9019002602de149725f4f4b2effd42
Ethan D. Twardy <ethan.twardy@gmail.com>
2021-11-04 17:19:27 -0500

    Implement git-touch application

---
+++ README.md
@@ -0,0 +1,20 @@
+# git-touch, A Convenient Git Extension
+
+This application is almost too stupidly simple to exist, but it's a small
+quality-of-life improvement for my development workflow (and hopefully others,
+as well).
+
+I write the following two commands a lot:
+
+```
+$ touch source/myfile.c
+$ git add source/myfile.c
+```
+
+With this application installed, I can just run:
+
+```
+$ git touch source/myfile.c
+```
+
+Yep, that's all it does. Enjoy!
---
+++ git-touch.c
@@ -0,0 +1,123 @@
+///////////////////////////////////////////////////////////////////////////////
+// NAME:            git-touch.c
+//
+// AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
+//
+// DESCRIPTION:     Entrypoint for the git-touch application
+//
+// CREATED:         11/04/2021
+//
+// LAST EDITED:     11/04/2021
+////
+
+#include <argp.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+const char* argp_program_version = "git-touch 0.1.0";
+const char* argp_program_bug_address = "<ethan.twardy@gmail.com>";
+
+static char doc[] = "Create and track changes to files with a single command";
+static char args_doc[] = "filename";
+static const int NUMBER_OF_ARGUMENTS = 1;
+
+static const char* GIT = "git";
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    char** filename = state->input;
+    switch (key) {
+    case ARGP_KEY_ARG:
+        if (state->arg_num >= NUMBER_OF_ARGUMENTS) {
+            argp_usage(state);
+        }
+
+        *filename = arg;
+        break;
+    case ARGP_KEY_END:
+        if (state->arg_num < NUMBER_OF_ARGUMENTS) {
+            argp_usage(state);
+        }
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+static struct argp argp = { 0, parse_opt, args_doc, doc, 0, 0, 0 };
+
+int git_in_dir(const char* path) {
+    struct dirent* entry = NULL;
+    DIR* directory = opendir(path);
+    char error_message[80];
+    if (NULL == directory) {
+        strerror_r(errno, error_message, sizeof(error_message));
+        fprintf(stderr, "Cannot open directory %s: %s\n", path, error_message);
+        return errno;
+    }
+
+    while (NULL != (entry = readdir(directory))) {
+        if (0 == strcmp(entry->d_name, GIT)) {
+            return 0;
+        }
+    }
+
+    closedir(directory);
+    return ENOENT;
+}
+
+int find_git(char* path, size_t path_length) {
+    char* env_path = getenv("PATH");
+    char* dir = NULL;
+    char* saveptr = NULL;
+
+    for (dir = strtok_r(env_path, ":", &saveptr); dir;
+         dir = strtok_r(NULL, ":", &saveptr)) {
+        int result = git_in_dir(dir);
+        if (0 == result) {
+            size_t dir_length = strlen(dir);
+            size_t git_length = strlen(GIT);
+            if (path_length < dir_length + 1 + git_length + 1) {
+                return ERANGE;
+            }
+
+            memcpy(path, dir, dir_length);
+            path[dir_length] = '/';
+            memcpy(path + dir_length + 1, GIT, git_length);
+            return 0;
+        } else if (ENOENT == result) {
+            continue; // Not found, but keep going
+        } else {
+            return result; // Error occurred
+        }
+    }
+
+    return ENOENT;
+}
+
+int main(int argc, char** argv) {
+    char* filename;
+    argp_parse(&argp, argc, argv, 0, 0, &filename);
+
+    // Create the file
+    FILE* file = fopen(filename, "w");
+    fclose(file);
+
+    // Locate git in PATH
+    char git_path[PATH_MAX];
+    int result = find_git(git_path, PATH_MAX);
+    if (0 != result) {
+        perror("Couldn't find git executable in path");
+        return errno;
+    }
+
+    char* args[] = { git_path, "add", filename, NULL };
+    char* envp[] = { NULL };
+    execve(git_path, args, envp);
+}
+
+///////////////////////////////////////////////////////////////////////////////
